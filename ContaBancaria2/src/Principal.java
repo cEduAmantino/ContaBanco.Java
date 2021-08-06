6b5033d4c30409a10ecc5e0be65469462dea2ac3
Claudio Eduardo <Claudio Eduardo@ClaudioEduardo>
2021-08-06 09:27:31 -0300

    Adicionado alguns comandos para retornar valores

--- ContaBancaria2/src/Principal.java
+++ ContaBancaria2/src/Principal.java
@@ -1,10 +1,21 @@
 
 public class Principal {
 	public static void main(String[] args) {
+	
+//---Comandos---		
+//abrirConta
+//fecharConta
+//depositar
+//sacar
+//pagarMensalidade
+		
 		ContaBanco Pessoa1 = new ContaBanco();
 		Pessoa1.setDono("Alvaro");
 		Pessoa1.setNumConta(222);
 		Pessoa1.abrirConta("CC");
+		Pessoa1.depositar(100);
+		Pessoa1.sacar(120);
+		Pessoa1.pagarMensalidade();
 			
 		Pessoa1.estadoAtual();
 		
